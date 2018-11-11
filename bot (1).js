726d69d90727978908e9831c395507202668457f
mixgamer159753 <44775482+mixgamer159753@users.noreply.github.com>
2018-11-11 19:00:21 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
  });
 
  
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTA4NzM5ODgxMTY2NTY5NDcz.DsDoug.OW8asOh49Ry96KV9Kky9ljU5R9A);
